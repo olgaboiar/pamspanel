1da5768767fcb90a6f64522cb4dc17e9c78d7ac0
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-02 15:39:28 -0400

    created database

---
+++ db/migrate/20180502193526_create_cohorts.rb
@@ -0,0 +1,15 @@
+class CreateCohorts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cohorts do |t|
+      t.string :name
+      t.string :type
+      t.string :schedule
+      t.string :classroom
+      t.integer :course_id
+      t.date :start_date
+      t.integer :instructor_id
+
+      t.timestamps
+    end
+  end
+end
