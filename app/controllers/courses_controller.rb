f4c33388ca5d15dad677b1398ef93f66115fcf76
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-06 15:10:04 -0400

    added creating new course form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,10 +2,38 @@ class CoursesController < ApplicationController
 
     def index
         @courses = Course.all
-
     end
 
     def show
-        puts "hello course"
+        @course = Course.find(params[:id])
     end
+
+    def edit
+        @course = Course.find(params[:id])
+    end
+
+    def update
+        @course = Course.find(params[:id])
+        @course.update(course_params)
+        redirect_to '/courses'
+    end
+
+    def destroy
+        Course.find(params[:id]).destroy
+        redirect_to '/courses'
+    end
+
+    def new 
+        @course = Course.new
+    end
+
+    def create 
+        Course.create(course_params)
+        redirect_to '/courses'
+    end
+
+    def course_params
+        params.require(:course).permit(:title, :hours, :description, :picture, :id)
+      end
 end
+
