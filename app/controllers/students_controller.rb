9fc490b9a93ce9af5c51379c9dad08f4b23611fe
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-07 12:07:16 -0400

    completed create new functionality for all instances

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,38 @@
 class StudentsController < ApplicationController
 
     def index
-        puts "test student"
-
+        @students = Student.all
     end
 
     def show
-        puts "hello student"
+        @student = Student.find(params[:id])
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+        redirect_to '/students'
+    end
+
+    def destroy
+        Student.find(params[:id]).destroy
+        redirect_to '/students'
+    end
+
+    def new 
+        @student = Student.new
+    end
+
+    def create 
+        Student.create(student_params)
+        redirect_to '/students'
+    end
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :email, :phone, :user_id, :cohort_id, :birthday, :education, :id)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,39 @@
 class UsersController < ApplicationController
 
     def index
-        puts "test user"
-
+        @users = User.all
     end
 
     def show
-        puts "hello user"
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to '/users'
+    end
+
+    def destroy
+        User.find(params[:id]).destroy
+        redirect_to '/users'
+    end
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        User.create(user_params)
+        redirect_to '/users'
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :role, :userpic, :id)
     end
 end
+
