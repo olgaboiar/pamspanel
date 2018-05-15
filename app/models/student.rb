9d335889566ed6d72b63ae067a51668323ee61b9
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-15 11:14:39 -0400

    added modal functionality fot ajax updating

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
     belongs_to :cohort
+    validates :first_name, :presence => true
+    validates :last_name, :presence => true
+    validates :birthday, :presence => true
 end
