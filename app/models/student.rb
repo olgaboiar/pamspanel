ae5b6994dbbfdfaea1b5efec21fbc710e880dc9f
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-15 14:03:25 -0400

    fixed that new student bug

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-    belongs_to :cohort
+    belongs_to :cohort, optional: true
     validates :first_name, :presence => true
     validates :last_name, :presence => true
     validates :birthday, :presence => true
